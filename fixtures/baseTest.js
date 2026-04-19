b7a1a078691f322886d798ea8c1c060055b44ce6
Phanindramylaru <Pmylaru@gmail.com>
2026-04-19 19:45:56 +0530

    attached allure report

--- fixtures/baseTest.js
+++ fixtures/baseTest.js
@@ -1,20 +1,32 @@
 const base = require('@playwright/test');
 const screenshot = require('../utils/screenshotsutil');
 const SignupActions = require('../actions/partnersignupactions');
+const { allure } = require('allure-playwright');
 
 const test = base.test.extend({
 
-    signup: async ({ page }, use,testInfo) => {
+    signup: async ({ page }, use, testInfo) => {
+
         const signup = new SignupActions(page);
+
         await use(signup);
-        if(testInfo.status!= testInfo.expectedStatus)
-        {
-            const path = await screenshot.takescreenshot(page,testInfo.title);
-            await testInfo.attach('screenshot',{
-                path : path,
-                contentType : 'image/png'
-            }
-            )
+
+
+        if (testInfo.status !== testInfo.expectedStatus) {
+
+            const path = await screenshot.takescreenshot(page, testInfo.title);
+
+            await testInfo.attach('screenshot', {
+                path: path,
+                contentType: 'image/png'
+            });
+
+
+            await allure.attachment(
+                'Failure Screenshot',
+                await page.screenshot(),
+                'image/png'
+            );
         }
     }
 
